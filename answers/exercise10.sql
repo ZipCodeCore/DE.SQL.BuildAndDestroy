0a40a88193b3455102ba04321e11c0c6b39d9af9
Chuck <chuck@zipcodes-MacBook-Pro-chuck.local>
2021-06-23 22:44:03 -0400

    5 more till done

---
+++ answers/exercise10.sql
@@ -0,0 +1,1 @@
+ALTER TABLE Users ADD COLUMN MiddleName varchar(255), ADD COLUMN NickName varchar(255), ADD COLUMN Email varchar(255), ADD COLUMN Suffix varchar(64), ADD COLUMN BadgeID varchar(255);
---
+++ answers/exercise13.sql
@@ -0,0 +1,1 @@
+ALTER TABLE Users CHANGE FirstName First_Name varchar(255), CHANGE LastName Last_Name varchar(255);
